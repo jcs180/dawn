f8deb949da8e3408dbb8d50340310d940c44e625
Jordan Crow-Stewart <77353771+jcs180@users.noreply.github.com>
2024-11-24 15:48:57 +0000

    checkpoint

--- assets/featured-collection-slider.js
+++ assets/featured-collection-slider.js
@@ -8,18 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const slider = document.createElement('div');
 
     slider.classList.add('slick-slider');
-    for (product of products) {
-      const slide = document.createElement('div');
-      slide.classList.add('slick-slide');
-      slide.innerHTML = `
-              <a href="${product.url}">
-                <img src="${product.image}" alt="${product.title}" width="${product.width}" height="${product.height}">
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
-              </a>
-            `;
-      slider.appendChild(slide);
-    }
 
     sliderContainer.appendChild(slider);
 
